c7ceaa42b91afe2848dfe6c6c6a412a0d695f70d
Richard Tafurth-Garcia <r.tafurthgarc@hftm.ch>
2023-07-24 10:45:33 +0200

    Won't use freeze ever again -> there is probably an issue in the build process that prevents me from running the board ? So im gonna rely on the current firmware

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+shopt -s globstar
+set -e
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+MANIFEST="$(pwd)/manifest.py"
 
 mpremote cp main.py :main.py
 mpremote cp camera.py :camera.py
 mpremote cp ds18b20.py :ds18b20.py
 mpremote cp -r static/ :
 mpremote cp -r templates/ :
+
+rm -rf lib/
+echo -e "${GREEN}Installing packages...${NC}"
+git clone https://github.com/miguelgrinberg/microdot/ lib/microdot
+cd lib/microdot/src
+mpremote cp *.py :
+cd ../libs/common
+mpremote cp -r utemplate/ :
+
 mpremote soft-reset
